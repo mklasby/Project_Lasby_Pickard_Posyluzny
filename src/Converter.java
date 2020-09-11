7e9119cdf0904005737485a1591a097b84f339e8
Mike Lasby <mklasby@ucalgary.ca>
2020-09-11 11:55:10 -0600

    completed main

--- src/Converter.java
+++ src/Converter.java
@@ -1,34 +1,33 @@
 
 public class Converter {
 
-	/* @Authors:
-	 * Student 1
-	 * Joshua Posyluzny
-	 * Student 3
+	/*
+	 * @Authors: Mike Lasby Joshua Posyluzny Student 3
 	 * 
 	 */
-	
-	private double celsiusToFahrenheit(double C)
-	{
+
+	private double celsiusToFahrenheit(double C) {
 		// TODO: THe third student will implement this method
 		return 0;
 	}
-	
-	private double fahrenheitToCelsius(double F)
-	{
+
+	private double fahrenheitToCelsius(double F) {
 		double celsius = 0;
 		double fahrenheit = F;
-		celsius = ((fahrenheit - 32)*(5/9));
+		celsius = ((fahrenheit - 32) * (5.0 / 9.0));
 		return celsius;
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		Converter conv = new Converter();
-		
-		double fahr = 180;
+		double fahr = 250;
 		double fToC = conv.fahrenheitToCelsius(fahr);
-		System.out.println(fToC);
+		System.out.printf("%.2f degrees F is %.2f degrees C\n", fahr, fToC);
+		double cels = 180;
+		double cToF = conv.celsiusToFahrenheit(cels);
+		System.out.printf("%.2f degrees C is %.2f degrees F\n", cels, cToF);
+
 	}
 
 }
